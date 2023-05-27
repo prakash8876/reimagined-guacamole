155f0c13456bc1170f679de467018ade61d67937
prakash8876 <prakash8876@gmail.com>
2023-05-27 20:32:57 +0530

    jetty-test-app added, initial commit

---
+++ jetty-testing-app/src/main/java/com/example/app/JettyTestingAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JettyTestingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JettyTestingAppApplication.class, args);
+	}
+
+}
---
+++ jetty-testing-app/src/test/java/com/example/app/JettyTestingAppApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.app;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JettyTestingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+		Assertions.assertTrue(Boolean.TRUE);
+	}
+
+}
